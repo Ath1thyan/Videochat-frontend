d5bfcdd0170e7041bc01a98c953491907be6e9d0
Ath1thyan <athitasks@gmail.com>
2024-05-08 13:58:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Card, Dropdown, Space } from 'antd';
+import { MoreOutlined } from '@ant-design/icons';
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    return (
+        <div className='body'>
+            <div className='header'>
+                <h1 className='header-title'>Video Chat</h1>
+            </div>
+
+            <VideoPlayer />
+
+            <Options>
+                <Notifications />
+            </Options>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles"; // Correct import
+import { ContextProvider } from "./SocketContext";
+import "./styles.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const theme = createTheme();
+createRoot(document.getElementById('root')).render(
+    <ContextProvider>
+      <ThemeProvider theme={theme}> {/* Wrapped the App inside ThemeProvider */}
+          <App />
+      </ThemeProvider>
+    </ContextProvider>, 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
